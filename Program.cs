5e006d429ad29d937fdd784b5f3fad6e8d74d6cf
𝐇𝐞𝐛𝐚 🍒☆ <163648306+heba-mustafa59@users.noreply.github.com>
2025-09-05 14:58:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,19 +8,32 @@ namespace APP1
 {
     internal class Program
     {
+        delegate void d1 (int number);
+        static bool isodd(int number) => (number % 2 == 1 || number %2==-1);
+        static void displyy(List<int> numbers)
+        {
+            foreach (int number in numbers)
+                if (isodd(number)) 
+                    Console.WriteLine(number);
+        } 
+       
+        static bool isnegative(int number) => (number < 0); 
+        static void displynegative (List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (isnegative(number)) Console.WriteLine(number); 
+            } 
+        }
+        
         static void Main(string[] args)
         {
-            
-          Account a1 = new Account();
-            a1.Name = "Heba";
-            a1.Balance= 100;
+            List<int> numbers = new List<int>() { 3,10,-9, 20, 30, 40, 50 ,-3};
+            displynegative(numbers);
+            Console.WriteLine("===========");
+            displyy(numbers);
 
-            Account a2 = new Account();
-            a2.Name = "huda";
-            a2.Balance= 100;
-             
-            
-            Console.WriteLine(a1>a2);
+
 
         }
     }
